fe12549f0dc2a0c99a7929086b35b89d5d30cd18
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-04 16:47:39 +0100

    js: Use querySelector to simplify code

--- js/current_page_underline.js
+++ js/current_page_underline.js
@@ -8,11 +8,10 @@ function addUnderlineIfLinksToCurrentPage(element)
 {
     if (!element) { return; }
 
-    const anchor = element.getElementsByTagName("a")[0];
+    const anchor = element.querySelector("a");
     if (!anchor) { return; }
 
     const anchor_pathname = new URL(anchor.href).pathname;
-
     if (window.location.pathname == anchor_pathname || is_on_index_with_empty_pathname(anchor_pathname))
     {
         anchor.classList.add("underline");
@@ -21,7 +20,7 @@ function addUnderlineIfLinksToCurrentPage(element)
 
 window.addEventListener("load", () =>
 {
-    const nav_list = document.getElementsByClassName("nav-list")[0];
+    const nav_list = document.querySelector(".nav-list");
     if (!nav_list) { return; }
 
     const nav_list_elements = nav_list.getElementsByTagName("li");
