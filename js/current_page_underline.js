86c11f1e2b0cf30df7734c63c4adc6ef73d0bd4a
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-05 18:22:08 +0100

    Reformat Files

--- js/current_page_underline.js
+++ js/current_page_underline.js
@@ -1,33 +1,31 @@
-function is_on_index_with_empty_pathname(pathname)
-{
+export { is_on_index_with_empty_pathname, addUnderlineIfLinksToCurrentPage };
+
+function is_on_index_with_empty_pathname(pathname) {
     if (window.location.pathname != "/") return false;
     return pathname == "/index.html";
 }
 
-function addUnderlineIfLinksToCurrentPage(element)
-{
+function addUnderlineIfLinksToCurrentPage(element) {
     if (!element) { return; }
 
     const anchor = element.querySelector("a");
     if (!anchor) { return; }
 
     const anchor_pathname = new URL(anchor.href).pathname;
-    if (window.location.pathname == anchor_pathname || is_on_index_with_empty_pathname(anchor_pathname))
-    {
+    if (window.location.pathname == anchor_pathname || is_on_index_with_empty_pathname(anchor_pathname)) {
         anchor.classList.add("underline");
     }
 }
 
-window.addEventListener("load", () =>
-{
+window.addEventListener("load", () => {
     const nav_list = document.querySelector(".nav-list");
     if (!nav_list) { return; }
 
     const nav_list_elements = nav_list.getElementsByTagName("li");
     if (!nav_list_elements) { return; }
-    
-    for (let current_element of nav_list_elements)
-    {
+
+    for (let current_element of nav_list_elements) {
         addUnderlineIfLinksToCurrentPage(current_element);
     }
 });
+
